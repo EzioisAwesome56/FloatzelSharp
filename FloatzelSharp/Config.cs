8fc2cb86ca9d53830c9fb07e757451dcce09022a
Jon <jedijon567@gmail.com>
2020-03-10 18:31:49 -0400

    whoops

---
+++ FloatzelSharp/Config.cs
@@ -0,0 +1,31 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FloatzelSharp
+{
+    internal class Config
+    {
+        // stolen from kekbot...again
+        // ah well
+        [JsonProperty("token")]
+        public string Token { get; private set; }
+
+        private static Config _instance;
+
+        // lifted from kekbot..?
+        public static async Task<Config> Get()
+        {
+            if (_instance == null)
+            {
+                using var fs = File.OpenRead("../../../../config.json");
+                using var sr = new StreamReader(fs, new UTF8Encoding(false));
+                return _instance = JsonConvert.DeserializeObject<Config>(await sr.ReadToEndAsync());
+            }
+            else return _instance;
+        }
+    }
+}
---
+++ FloatzelSharp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading.Tasks;
+using DSharpPlus;
+
+namespace FloatzelSharp
+{
+    class Program
+    {
+        static DiscordClient discord;
+        static void Main(string[] args)
+        {
+            MainAsync(args).ConfigureAwait(false).GetAwaiter().GetResult();
+        }
+
+        static async Task MainAsync(string[] args)
+        {
+            var config = await Config.Get();
+            discord = new DiscordClient(new DiscordConfiguration
+            {
+                Token = config.Token,
+                TokenType = TokenType.Bot
+            });
+            discord.MessageCreated += async e =>
+            {
+                if (e.Message.Content.ToLower().StartsWith("ping"))
+                    await e.Message.RespondAsync("pong!");
+            };
+
+            await discord.ConnectAsync();
+            await Task.Delay(-1);
+
+        }
+    }
+}
