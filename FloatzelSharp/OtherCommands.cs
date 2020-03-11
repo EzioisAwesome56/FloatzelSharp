0a6e149eb3757322f35ec8cb652e8cadf560e123
Jon <jedijon567@gmail.com>
2020-03-10 20:52:06 -0400

    add a few new commands

--- FloatzelSharp/OtherCommands.cs
+++ FloatzelSharp/OtherCommands.cs
@@ -5,6 +5,7 @@ using System.Text;
 using FloatzelSharp.help;
 using DSharpPlus.CommandsNext.Attributes;
 using System.Threading.Tasks;
+using System.Diagnostics;
 
 namespace FloatzelSharp
 {
@@ -20,6 +21,19 @@ namespace FloatzelSharp
         {
             Random rand = new Random();
             await ctx.RespondAsync(rand.Next().ToString());
+        }
+        [Command("stats"), Description("shows states about the bot"), Category(Category.Other)]
+        public async Task stats(CommandContext ctx)
+        {
+            String stats = "```Floatzel Stats\n";
+            stats = stats + ".NET Version: " + Environment.Version.ToString() + "\n";
+            stats = stats + "OS Version: " + Environment.OSVersion.ToString() + "\n";
+            stats = stats + "Processor count: " + Environment.ProcessorCount.ToString() + "\n";
+            stats = stats + "64bit OS?: " + Environment.Is64BitOperatingSystem.ToString() + "\n";
+            stats = stats + "64bit process?: " + Environment.Is64BitProcess.ToString() + "\n";
+            stats = stats + "Memory Usage: " + ((Process.GetCurrentProcess().PrivateMemorySize64 / 1024) / 1024).ToString() + " MB\n";
+            stats = stats + "```";
+            await ctx.RespondAsync(stats);
         }
 
 
