319f50a46ee6fdab19999fb5427074df7f10f4f3
Jon <jedijon567@gmail.com>
2020-03-10 21:27:53 -0400

    start writing new database code

---
+++ FloatzelSharp/util/Database.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using RethinkDb.Driver;
+using RethinkDb.Driver.Net;
+
+namespace FloatzelSharp.util
+{
+    class Database
+    {
+        // init rethinkdb in its whole
+        public static RethinkDB r = RethinkDB.R;
+        public static Connection thonk;
+
+        // copy-paste from java: table names
+        private static String banktable = "bank";
+        private static String loantable = "loan";
+        private static String bloanperm = "bloan";
+        private static String stocktable = "stocks";
+        private static String stockbuy = "boughtstock";
+        private static String tweets = "tweets";
+        private static String tagperm = "gtagperm";
+        private static String tags = "tags";
+
+
+
+        public static void dbinit()
+        {
+            Connection.Builder builder = r.Connection().Hostname("localhost").Port(28015);
+            // connect
+            thonk = builder.Connect();
+            
+            Console.WriteLine("Floatzel is now loading EzioSoft RethinkDB Driver V2...");
+            // check if the database exists
+            if (!(bool) r.DbList().Contains("floatzel").Run(thonk)) {
+                // it doesnt exist! make that database!
+                Console.WriteLine("Database not detected! creating new database...");
+                r.DbCreate("floatzel").Run(thonk);
+                thonk.Use("floatzel");
+                Console.WriteLine("Creating tables...");
+                makeTables();
+                Console.WriteLine("Database created!");
+            } else {
+                thonk.Use("floatzel");
+                Console.WriteLine("Driver loaded!");
+            }
+        }
+
+
+        private static void makeTables() {
+            // run a bunch of rethink commands
+            r.TableCreate(banktable).Run(thonk);
+            r.TableCreate(loantable).Run(thonk);
+            r.TableCreate(bloanperm).Run(thonk);
+            r.TableCreate(stocktable).Run(thonk);
+            r.TableCreate(tweets).Run(thonk);
+            r.TableCreate(tagperm).Run(thonk);
+            r.TableCreate(tags).Run(thonk);
+            r.TableCreate(stockbuy).Run(thonk);
+        }
+    }
+}
