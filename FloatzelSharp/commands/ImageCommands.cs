b3bf34235726391d64f931291648ea15acf9d688
Jon <jedijon567@gmail.com>
2020-03-14 17:59:07 -0400

    dispose of stuff

--- FloatzelSharp/commands/ImageCommands.cs
+++ FloatzelSharp/commands/ImageCommands.cs
@@ -34,10 +34,16 @@ namespace FloatzelSharp.commands {
             Stream stream = await client.OpenReadTaskAsync(new Uri(ctx.User.AvatarUrl));
             IMagickImage ava = new MagickImage(stream);
             Stream streamm = new MemoryStream();
-            IMagickImage test2 = new MagickImage(MagickColor.FromRgba(255, 0, 0, 255), ava.Width, ava.Height);
-            test2.Format = MagickFormat.Png64;
-            test2.Composite(ava, 50, 50, CompositeOperator.SrcOver);
-            await ctx.RespondWithFileAsync("test.png", new MemoryStream(test2.ToByteArray()), "test");
+            IMagickImage img = new MagickImage(MagickColor.FromRgba(255, 0, 0, 255), ava.Width, ava.Height);
+            img.Format = MagickFormat.Png64;
+            img.Composite(ava, 50, 50, CompositeOperator.SrcOver);
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("test.png", memory, "test");
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
 
         [Command("wall"), Description("make a wall from an attachment"), Category(Category.Image)]
@@ -64,7 +70,13 @@ namespace FloatzelSharp.commands {
             img.Resize(512, 512);
             img.Distort(DistortMethod.Perspective,  0, 0, 57, 42, 0, 128, 63, 130, 128, 0, 140, 60, 128, 128, 140, 140);
             img.Format = MagickFormat.Png64;
-            await ctx.RespondWithFileAsync("wall.png", new MemoryStream(img.ToByteArray()));
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("wall.png", memory);
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
 
         [Command("swirl"), Description("swirls an image. Incredible"), Category(Category.Image)]
@@ -89,7 +101,13 @@ namespace FloatzelSharp.commands {
             IMagickImage img = new MagickImage(stream);
             img.Swirl((double)180);
             img.Format = MagickFormat.Png64;
-            await ctx.RespondWithFileAsync("swirl.png", new MemoryStream(img.ToByteArray()));
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("swirl.png", memory);
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
 
         [Command("jpeg"), Description("make any image into an authentic JPEG"), Category(Category.Image)]
@@ -115,7 +133,13 @@ namespace FloatzelSharp.commands {
             img.Format = MagickFormat.Jpeg;
             img.Quality = 0;
             img.Quality = -5;
-            await ctx.RespondWithFileAsync("jpeg.jpg", new MemoryStream(img.ToByteArray()));
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("jpeg.jpg", memory);
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
 
         [Command("pixel"), Description("reduces an image to 1x1 in size"), Category(Category.Image)]
@@ -141,7 +165,13 @@ namespace FloatzelSharp.commands {
             img.Resize(1, 1);
             img.Resize(200, 200);
             img.Format = MagickFormat.Png64;
-            await ctx.RespondWithFileAsync("jpeg.jpg", new MemoryStream(img.ToByteArray()));
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("jpeg.jpg", memory);
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
 
         [Command("explode"), Description("explodes an image"), Category(Category.Image)]
@@ -166,7 +196,13 @@ namespace FloatzelSharp.commands {
             IMagickImage img = new MagickImage(stream);
             img.Implode(-2, PixelInterpolateMethod.Bilinear);
             img.Format = MagickFormat.Png64;
-            await ctx.RespondWithFileAsync("explode.png", new MemoryStream(img.ToByteArray()));
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("explode.png", memory);
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
 
         [Command("implode"), Description("implodes an image"), Category(Category.Image)]
@@ -191,7 +227,13 @@ namespace FloatzelSharp.commands {
             IMagickImage img = new MagickImage(stream);
             img.Implode(1, PixelInterpolateMethod.Bilinear);
             img.Format = MagickFormat.Png64;
-            await ctx.RespondWithFileAsync("implode.png", new MemoryStream(img.ToByteArray()));
+            var memory = new MemoryStream(img.ToByteArray());
+            await ctx.RespondWithFileAsync("implode.png", memory);
+            // Dispose of everything used
+            await memory.DisposeAsync();
+            await stream.DisposeAsync();
+            img.Dispose();
+            //client.Dispose();
         }
     }
 }
