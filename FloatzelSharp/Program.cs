04e827f3ea67e11e784396825cee8d55cab9cb37
Jon <jedijon567@gmail.com>
2020-07-04 21:46:39 -0400

    change this

--- FloatzelSharp/Program.cs
+++ FloatzelSharp/Program.cs
@@ -197,7 +197,7 @@ namespace FloatzelSharp
         // copy-paste from Floatzel: Java Edition. GAMES
         // imo they are too iconic to be thrown away entirely
         static string[] games = { "Windows 7", "mspaint.exe", "&help", "Nintendo Gamecube", "GIMP startup",
-            "fucking weed", "Sonic 09", "Dying in a hole", "gay xd", "Exploding North Korea", "Being a terrorist", "BSOD",
+            "fucking weed", "Sonic 09", "Dying in a hole", "waffle man", "Exploding North Korea", "Being a terrorist", "BSOD",
             "Extra gay", "memerino and the dank machine", "Dankform", "Half Life 4", "18 is a god", "SaltyPepper", "@here",
             "Simpsons", "Explosive cheese", "Being a bot", "hi Cublex", "Pentium 3", "meme machine", "18 is bisexual", "ezio is bi",
             "PyCharm", "Rectangle Gay", "Making the frogs gay", "eirrac", "what the fuck is a hedgehog", "heck you blur", "enitsirhc", "fuck me harder daddy",
